62bbed6e2d5a8039f60ae7518b6cb8dc8a6cce47
Prathyakshanaa <112697187+Prathyakshanaa@users.noreply.github.com>
2023-10-05 08:20:17 +0530

    Update assignment11.cpp

--- DAY 2/assignment11.cpp
+++ DAY 2/assignment11.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
 int main() {
-    int oldest_painting = 0; // Initialize to 0 years (assuming 0 represents unknown)
-
+    int oldest_painting = 0; 
     int painting1 = 4200;
     int painting2 = 8500;
     int painting3 = 1000;
     int painting4 = 1300;
-
     if (painting1 > painting2 && painting1 > painting3 && painting1 > painting4) {
         oldest_painting = painting1;
     }
@@ -20,9 +18,7 @@ int main() {
     else {
         oldest_painting = painting4;
     }
-
     std::cout << "The oldest cave painting is " << oldest_painting << " years old." << std::endl;
-
     return 0;
 }
 
