bc689e567babe57a9c73558b1d250e00d36a46c6
Prathyakshanaa <112697187+Prathyakshanaa@users.noreply.github.com>
2023-10-05 08:21:13 +0530

    Update assignment1.cpp

--- DAY 2/assignment1.cpp
+++ DAY 2/assignment1.cpp
@@ -5,28 +5,20 @@ private:
     double costPrice;
     double sellingPrice;
     double profit;
-
 public:
-    // Default constructor
     PlasticTransaction() {
-        costPrice = 10.0;  // Cost price of 1 kg plastic
-        sellingPrice = 12.0;  // Selling price of 1 kg plastic
+        costPrice = 10.0;  
+        sellingPrice = 12.0;  
         profit = sellingPrice - costPrice;
     }
-
     double getProfit() {
         return profit;
     }
 };
-
 int main() {
-    // Create an instance of the PlasticTransaction class
     PlasticTransaction transaction;
-
-    // Calculate and display the profit
     double profit = transaction.getProfit();
     std::cout << "Kiran earns " << profit << " rupees by selling 1 kg of plastic." << std::endl;
-
     return 0;
 }
 
